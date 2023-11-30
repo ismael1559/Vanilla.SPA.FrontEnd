b65e8609dd71f2f43e7553cd9bf8efa0e4dc9468
ismael1559 <ismael.correa@aluno.senac.rio>
2023-11-30 17:59:57 -0300

    aula29

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,15 @@
 function myHome() {
     changeTitle();
+    console.log('home funciona')
+
+    getALLItems();
+
 }
+
+function getALLItems(){
+    console.log("funciona");
+    return false;
+}
+
+
 $(document).ready(myHome);
